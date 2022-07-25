28efa2eccf0cb6a2862b45f7f0c94f9c4da1e0af
Niv Caspi <niv0caspi@gmail.com>
2022-07-25 13:44:30 +0300

    front end

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function changeBackgroundColor(color){
-	document.body.style.backgroundColor = color;
-
+/**function changeBackgroundColor(color){
+	
+	document.body.style.backgroundColor = 'lightblue';
+	if(document.body.style.backgroundColor != color){
+		document.body.style.backgroundColor = 'green';
+	}
+}**/
+function changeBackgroundColor() {
+  if(document.body.style.backgroundColor == "green"){
+  	document.body.style.backgroundColor = "red";
+  }
+  else{
+  	document.body.style.backgroundColor = "green";
+  }
+  
 }
